f2faac1c27b9ffeec1a6532f95ccfdeaddde66d3
itiskamrul <kamrul74k@gmail.com>
2020-05-01 15:57:01 +0600

    compressed file added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./animate.css";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './animate.css';
 
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import NavBar from "./Nav";
-import Contact from "./components/Contact";
-import Main from "./components/Main";
-import MySkills from "./components/MySkills";
-import UnderConstruct from "./components/UnderConstruct";
+import NavBar from './Nav';
+import Contact from './components/Contact';
+import Main from './components/Main';
+import MySkills from './components/MySkills';
+import UnderConstruct from './components/UnderConstruct';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Router>
         <NavBar />
-        <div className="container">
-          <Route exact path="/" component={Main} />
-          <Route exact path="/skills" component={MySkills} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/Components" component={UnderConstruct} />
-          <Route exact path="/about" component={UnderConstruct} />
+        <div className='container'>
+          <Route exact path='/' component={Main} />
+          <Route exact path='/skills' component={MySkills} />
+          <Route exact path='/contact' component={Contact} />
+          <Route exact path='/Components' component={UnderConstruct} />
+          <Route exact path='/about' component={UnderConstruct} />
         </div>
       </Router>
     </div>
--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-import { Collapse, Navbar, NavbarToggler, Nav } from "reactstrap";
+import { Collapse, Navbar, NavbarToggler, Nav } from 'reactstrap';
 
-const NavBar = (props) => {
+const NavBar = props => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div className="nav-container border">
-      <Navbar light expand="md">
-        <div className="container">
-          <Link className=" animated zoomIn navbar-brand" to="/">
+    <div className='nav-container border'>
+      <Navbar light expand='md'>
+        <div className='container'>
+          <Link className='   navbar-brand' to='/'>
             DeveloperKamrul
           </Link>
 
           <NavbarToggler onClick={toggle} />
           <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <li className="nav-item animated zoomIn">
-                <Link className="nav-link" to="/">
+            <Nav className='ml-auto' navbar>
+              <li className='nav-item '>
+                <Link className='nav-link' to='/'>
                   Home
                 </Link>
               </li>
-              <li className="nav-item animated zoomIn">
-                <Link className="nav-link" to="/Components">
+              <li className='nav-item '>
+                <Link className='nav-link' to='/Components'>
                   Components
                 </Link>
               </li>
-              <li className="nav-item animated zoomIn">
-                <Link className="nav-link" to="/skills">
+              <li className='nav-item '>
+                <Link className='nav-link' to='/skills'>
                   MySkills
                 </Link>
               </li>
-              <li className="nav-item animated zoomIn">
-                <Link className="nav-link" to="/contact">
+              <li className='nav-item '>
+                <Link className='nav-link' to='/contact'>
                   Contact
                 </Link>
               </li>
-              <li className="nav-item animated zoomIn">
-                <Link className="nav-link" to="/about">
+              <li className='nav-item'>
+                <Link className='nav-link' to='/about'>
                   About
                 </Link>
               </li>
